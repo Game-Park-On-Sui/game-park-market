5166a7f8c795a02fb4f25243e8e124435e9f2da8
Zcy <zcy_chao1024@163.com>
2025-03-18 15:03:49 +0800

    bind component(add password)

--- src/components/bind.tsx
+++ src/components/bind.tsx
@@ -1,22 +1,48 @@
 'use client'
 
-import {ChangeEvent, Dispatch, SetStateAction, useState} from "react";
+import {ChangeEvent, Dispatch, SetStateAction, useEffect, useState} from "react";
 
 export default function Bind({setIsBinding}: {setIsBinding: Dispatch<SetStateAction<boolean>>}) {
     const [userName, setUserName] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [confirmPassword, setConfirmPassword] = useState<string>("");
     const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    const checkInput = (str: string) => {
+        if (str.length > 20)
+            return false;
+        for (let i = 0; i < str.length; i++) {
+            const code = str[i];
+            if (!(code >= '0' && code <= '9' || code >= 'a' && code <= 'z' || code >= 'A' && code <= 'Z'))
+                return false;
+        }
+        return true;
+    }
+
     const changeUserName = (e: ChangeEvent<HTMLInputElement>) => {
         const newUserName = e.target.value.trim();
-        if (newUserName.length > 20)
+        if (!checkInput(newUserName))
             return;
-        for (let i = 0; i < newUserName.length; i++) {
-            const code = newUserName[i];
-            if (!(code >= '0' && code <= '9' || code >= 'a' && code <= 'z' || code >= 'A' && code <= 'Z'))
-                return;
-        }
         setUserName(newUserName);
-        setIsVisible(newUserName.length > 0);
     }
+
+    const changePassword = (e: ChangeEvent<HTMLInputElement>) => {
+        const newPassword = e.target.value.trim();
+        if (!checkInput(newPassword))
+            return;
+        setPassword(newPassword);
+    }
+
+    const changeConfirmPassword = (e: ChangeEvent<HTMLInputElement>) => {
+        const newPassword = e.target.value.trim();
+        if (!checkInput(newPassword))
+            return;
+        setConfirmPassword(newPassword);
+    }
+
+    useEffect(() => {
+        setIsVisible(userName.length > 0 && password.length > 0 && password === confirmPassword);
+    }, [userName, password, confirmPassword]);
 
     return (
         <div className="fixed w-full h-full z-50">
@@ -25,8 +51,16 @@ export default function Bind({setIsBinding}: {setIsBinding: Dispatch<SetStateAct
                 <div className="flex flex-col gap-6 items-center text-[#9d9d9d]">
                     <span className="text-2xl text-[#35a1f7] animate-pulse">Game Park On Sui</span>
                     <div className="flex flex-col gap-1 items-start">
-                        <span>UserName:</span>
-                        <input className={"focus:outline-none " + (userName.length > 0 ? "text-[#041f4b]" : "")} placeholder="UserName" value={userName} onChange={changeUserName} />
+                        <span>User Name:</span>
+                        <input className={"focus:outline-none " + (userName.length > 0 ? "text-[#041f4b]" : "")} placeholder="User Name" value={userName} onChange={changeUserName} />
+                    </div>
+                    <div className="flex flex-col gap-1 items-start">
+                        <span>Password:</span>
+                        <input className={"focus:outline-none"} placeholder="Password" type={"password"} value={password} onChange={changePassword} />
+                    </div>
+                    <div className="flex flex-col gap-1 items-start">
+                        <span>Confirm Password:</span>
+                        <input className={"focus:outline-none"} placeholder="Confirm Password" type={"password"} value={confirmPassword} onChange={changeConfirmPassword} />
                     </div>
                     <div className="flex flex-col gap-2 items-start">
                         <span>Sui Address:</span>
