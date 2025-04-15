0e6877cab36794b2e4f9f28de5eb1c8f83ae83ec
Zcy <zcy_chao1024@163.com>
2025-04-15 16:49:02 +0800

    add tradeType

--- src/components/trade/index.tsx
+++ src/components/trade/index.tsx
@@ -3,10 +3,12 @@
 import {useEffect, useState} from "react";
 import {JumpingCard} from "@/components/trade/cards";
 
-const tabTile = ["Jumping", "Soon..."];
+const TabTile = ["Jumping", "Soon..."];
+const TradeType = ["Buy", "Sell"];
 
 export default function Trade() {
     const [tab, setTab] = useState<number>(0);
+    const [tradeType, setTradeType] = useState<number>(0);
     const [tempArray, setTempArray] = useState<number[]>([]);
     useEffect(() => {
         const array: number[] = [];
@@ -18,23 +20,36 @@ export default function Trade() {
     return (
         <div className="h-[80vh] w-screen">
             <div className="h-full xl:px-32 2xl:px-96 mt-16 transition-all duration-1000">
-                <div className="relative h-full w-full p-5 border border-black">
-                    <div className="absolute -top-6 left-0">
+                <div className="relative h-full w-full p-5">
+                    <div className="absolute -top-6 left-0 w-full flex justify-between items-center">
                         <div className="flex gap-2 items-center border border-[#afb3b5] bg-[#afb3b5] rounded-full">
                             {
-                                tabTile.map((title, index) => {
+                                TabTile.map((title, index) => {
                                     return (
                                         <span key={index}
-                                              className={"w-24 rounded-full px-2 text-center cursor-pointer transition-all hover:opacity-100 " + (tab === index ? "bg-white" : "bg-[#afb3b5] opacity-60")}
-                                              onClick={() => setTab(index)}>
+                                              className={"w-24 rounded-full px-2 text-center cursor-pointer transition-all hover:opacity-100 " + (tradeType === index ? "bg-white" : "bg-[#afb3b5] opacity-60")}
+                                              onClick={() => setTradeType(index)}>
                                             {title}
                                         </span>
                                     );
                                 })
                             }
                         </div>
+                        <div className="flex gap-2 items-center border border-[#afb3b5] bg-[#afb3b5] rounded-full">
+                            {
+                                TradeType.map((type, index) => {
+                                    return (
+                                        <span key={index}
+                                              className={"w-24 rounded-full px-2 text-center cursor-pointer transition-all hover:opacity-100 " + (tab === index ? "bg-white" : "bg-[#afb3b5] opacity-60")}
+                                              onClick={() => setTab(index)}>
+                                            {type}
+                                        </span>
+                                    );
+                                })
+                            }
+                        </div>
                     </div>
-                    <div className="w-full h-full flex gap-2 flex-wrap content-start overflow-auto">
+                    <div className="w-full h-full flex gap-4 flex-wrap content-start overflow-auto">
                         {
                             tempArray.map((idx) => {
                                 return (
